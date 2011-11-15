0b89a4319c041ba76ef483659678a505962340cc
Markus Kahl <machisuji@googlemail.com>
2011-11-15 21:04:09 +0100

    first almost working version

---
+++ mask.scala
@@ -0,0 +1,46 @@
+object Masker {
+  def digits(chrs: Seq[Char]): Seq[Int] = chrs.map(_ - 48)
+  def digits(str: String): Seq[Int] = digits(str.toSeq)
+
+  def luhnCheck(digits: Seq[Int]): Boolean = {
+    digits.reverse.zipWithIndex.map { case (digit, index) =>
+      if (index % 2 == 1) digit * 2
+      else digit
+    }.flatMap { num =>
+      if (num < 10) Seq(num)
+      else Seq(1, num % 10)
+    }.sum % 10 == 0
+  }
+
+  def checkCC(number: Seq[Char]): Boolean = {
+    val ds = digits(number.filter(_.isDigit))
+    ds.size >= 14 && ds.size <= 16 && luhnCheck(ds)
+  }
+
+  def isCCDigit(chr: Char) = chr.isDigit || chr == '-' || chr == ' '
+
+  @scala.annotation.tailrec
+  def maskCC(prefix: String, number: Seq[Char], suffix: String, previousWasDigit: Boolean): String = {
+    if (checkCC(number)) return prefix ++ number.map(chr => if (chr.isDigit) 'X' else chr) ++ suffix
+    if (suffix.isEmpty) return prefix
+    val chr = suffix.head
+
+    if (previousWasDigit) {
+      if (isCCDigit(chr)) maskCC(prefix, number :+ chr, suffix.tail, true)
+      else                maskCC((prefix ++ number) :+ chr, Seq(), suffix.tail, false)
+    } else {
+      if (isCCDigit(chr)) maskCC(prefix, Seq(chr), suffix.tail, true)
+      else                maskCC((prefix ++ number) :+ chr, Seq(), suffix.tail, false)
+    }
+  }
+
+  def maskCC(str: String): String = maskCC("", Seq(), str, false)
+}
+
+object mask extends App {
+  val reader = new java.io.BufferedReader(new java.io.InputStreamReader(System.in))
+  Iterator.continually(reader.readLine).takeWhile(null ne).foreach(line =>
+    println(Masker.maskCC(line)))
+}
+
+mask main Array()
