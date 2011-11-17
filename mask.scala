4d618eff94c15f088acc0078a3a0226f34bd0d74
Markus Kahl <machisuji@googlemail.com>
2011-11-17 10:05:23 +0100

    mh

--- mask.scala
+++ mask.scala
@@ -8,9 +8,10 @@ object Masker {
       if (num < 10) Seq(num) else Seq(1, num % 10)
     }.sum % 10 == 0
   }
-  def checkCC(number: Seq[Char]): Boolean = {
+  def checkCC(number: Seq[Char]): Int = {
     val ds = number.filter(_.isDigit).map(_ - '0')
-    ds.size >= minDigits && ds.size <= maxDigits && luhnCheck(ds)
+    if (ds.size >= minDigits && ds.size <= maxDigits && luhnCheck(ds)) ds.size
+    else 0
   }
   def startsWithDigit(str: String) = str.headOption.exists(_.isDigit)
   def isCCDigit(chr: Char) = chr.isDigit || chr == '-' || chr.isWhitespace
@@ -19,10 +20,15 @@ object Masker {
   @scala.annotation.tailrec
   def split(parts: List[String], str: String, text: Boolean): List[String] = {
     val (part, rest) = str.span(c => if (text) !c.isDigit else isCCDigit(c))
-    if (rest.isEmpty) (part :: parts).reverse
-    else split(part :: parts, rest, !text)
+    val maskedPart = if (!text) maskCC(part) else part
+    if (rest.isEmpty) (maskedPart :: parts).reverse
+    else split(maskedPart :: parts, rest, !text)
   }
   def split(str: String): List[String] = split(Nil, str, !startsWithDigit(str))
+
+  def maskCC(str: String): String = {
+    
+  }
 
   def mask(str: String): String = split(str).mkString(" | ")
 }
